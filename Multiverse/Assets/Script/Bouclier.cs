7ac7c45d0215433971f061aaf656ce108b6d4a3c
Chloe <chloe.marce@efrei.net>
2022-03-20 18:40:47 +0100

    Added Tir & bouclier

---
+++ Multiverse/Assets/Script/Bouclier.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Bouclier : MonoBehaviour
+{
+    [SerializeField] private InputActionAsset actionAsset;
+    [SerializeField] private GameObject shield;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var activate = actionAsset.FindActionMap("XRI LeftHand").FindAction("Activate");
+        activate.Enable();
+        activate.performed += ShieldActivate;
+
+    }
+
+    private void ShieldActivate(InputAction.CallbackContext context)
+    {
+        //var bouclier = this.gameObject.transform.GetChild(1);
+        if (shield.gameObject.activeSelf == false)
+        {
+            shield.gameObject.SetActive(true);
+        }
+        else
+        {
+            shield.gameObject.SetActive(false);
+        }
+    }
+
+}
