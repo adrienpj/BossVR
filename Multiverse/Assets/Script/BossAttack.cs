51facb7f04b0af0e1f3f2aaf848d7ba13eea4b05
Olivier <olivier611.chen@gmail.com>
2022-04-13 00:34:44 +0200

    Boss attack

--- Multiverse/Assets/Script/BossAttack.cs
+++ Multiverse/Assets/Script/BossAttack.cs
@@ -4,35 +4,65 @@ using UnityEngine;
 
 public class BossAttack : MonoBehaviour
 {
-    [SerializeField] private GameObject fireball;
-    [SerializeField] float destroyTime = 5;
+    //[SerializeField] float destroyTime = 5;
     public Transform target;
     public float burst = 350f;
     public Transform boss;
+    public Transform player;
+    public Vector3 newPosition;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    public GameObject projectile;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
-        BossAtk();
+        //Vérifié la distance et l'attaque
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange);
+
+        StartCoroutine(ExampleCoroutine());
     }
 
-    void BossAtk()
+    IEnumerator ExampleCoroutine()
     {
-        if(boss.position == (target.position -transform.position))
+
+        yield return new WaitForSeconds(5);
+        if (playerInAttackRange && playerInSightRange) AttackPlayer();
+    }
+
+    private void AttackPlayer()
+    {
+        if (!alreadyAttacked)
         {
-            Debug.Log("AttackFireBall");
-            FireballActivate();
+            //Vector3 newPosition = new Vector3(transform.position.x - 14, transform.position.y + 11, transform.position.z);
+            Vector3 newPosition = new Vector3(boss.transform.position.x - 14, boss.transform.position.y + 11, boss.transform.position.z * 2);
+            ///Attaque code ici
+            //Rigidbody rb = Instantiate(projectile, new Vector3(-14, 25, 0), Quaternion.identity).GetComponent<Rigidbody>();
+            Rigidbody fireball = Instantiate(projectile, newPosition, Quaternion.identity).GetComponent<Rigidbody>();
+            fireball.AddForce(transform.forward * 15f, ForceMode.Impulse);
+            fireball.AddForce(transform.up * -5.5f, ForceMode.Impulse);
+            //Destroy(fireball.GetComponent<Rigidbody>(),destroyTime);
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+            
         }
     }
-
-    private void FireballActivate()
+    private void ResetAttack()
     {
-        //Création du projectile (boule de feu)
-        GameObject projectile = Instantiate(fireball, transform.position, transform.rotation);
-        //Donner une force et une direction
-        projectile.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * burst);
-        
-        //Le temps limite avant la destruction du projectile (boule de feu)
-        Destroy(projectile, destroyTime); 
+        alreadyAttacked = false;
     }
 }
