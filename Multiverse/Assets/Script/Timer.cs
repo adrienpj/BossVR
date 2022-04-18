9232301bd23f660a7e3c8516c605a1c3607afd74
Chloe <chloe.marce@efrei.net>
2022-04-18 16:30:01 +0200

    Timer + debug teleportation

---
+++ Multiverse/Assets/Script/Timer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Text>().text = Math.Round((time / 60), 2).ToString() + " minutes";
+
+        time = Time.time;
+    }
+}
